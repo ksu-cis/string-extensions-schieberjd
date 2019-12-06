d6d7d8028f0c59a79a36b9de67b1234eab69f4ba
schieberjd <schieberjd@F1092-41-2314>
2019-12-06 14:33:42 -0600

    In-class 12/6

---
+++ StringExtensions/StringExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] {' ', '.', '?', '\t', '\n'}, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string Capitalize(this String str)
+        {
+            string first = str[0].ToString().ToUpper();
+            return first + str.Substring(1);
+        }
+
+        public static void Decapitalize(this String str)
+        {
+            //Doesn't work
+            string first = str[0].ToString().ToLower();
+            str = first + str.Substring(1);
+        }
+
+        public static String Titleize(this String title)
+        {
+            List<string> articles = new List<string>()
+            {
+                "A",
+                "An",
+                "The"
+            };
+            List<string> parts = new List<string>(title.Split(" "));
+            if (articles.Contains(parts[0].ToLower()))
+            {
+                string first = parts[0];
+                parts.RemoveAt(0);
+                parts.Add(", " + first);
+                first = parts[0];
+                while (articles.Contains(first.ToLower()))
+                {
+                    parts.RemoveAt(0);
+                    parts.Add(first);
+                    first = parts[0];
+                }
+                string result = "";
+                foreach (string part in parts)
+                {
+                    result += part + " ";
+                }
+                return result;
+            }
+            return null;
+        }
+    }
+}
